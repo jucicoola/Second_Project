7d85394f287f49babd350ffa71ee0d7cdd5fcf89
Jackie Bang <aca1111@naver.com>
2026-02-10 10:04:35 +0900

    feat:setting-db변경

--- apps/trips/admin.py
+++ apps/trips/admin.py
@@ -5,5 +5,5 @@ from .models import Trip
 class TripAdmin(admin.ModelAdmin):
     list_display = ['name', 'country', 'city', 'start_date', 'end_date', 'user', 'created_at']
     list_filter = ['country', 'start_date']
-    search_fields = ['name', 'country', 'city', 'user__username']
+    search_fields = ['name', 'country__name', 'city', 'user__username']
     readonly_fields = ['created_at', 'updated_at']
--- config/urls.py
+++ config/urls.py
@@ -4,12 +4,11 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    # 루트 주소는 main 앱의 urls.py에서 관리하도록 넘깁니다.
-    path('', include('apps.main.urls')), 
-
-    # 각 기능을 담당하는 앱들
+        # 각 기능을 담당하는 앱들
     path('accounts/', include('apps.accounts.urls')),
     path('dashboard/', include('apps.dashboard.urls')),
     path('trips/', include('apps.trips.urls')),
     path('transactions/', include('apps.transactions.urls')),
+    # 루트 주소는 main 앱의 urls.py에서 관리하도록 넘깁니다.
+    path('', include('apps.main.urls')), 
 ]
